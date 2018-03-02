84dcd8f085f0505ce2f88862e32bda119c55ddb5
Ademir Burgic <aburgic@capeannenterprises.com>
2018-03-02 15:07:59 +0100

    methods

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -11,4 +11,30 @@ export class AuthService
                 console.log(error);
           });
     }
+
+    signinUser(user: User)
+    {
+        firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .catch(function(error) 
+        {
+            console.log(error);
+          });
+          
+    }
+
+    isAuthenticated()
+    {
+        var user = firebase.auth().currentUser;
+
+        if (user) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    logout()
+    {
+        firebase.auth().signOut();
+    }
 }
