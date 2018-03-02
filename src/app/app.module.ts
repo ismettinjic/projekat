7d655845db227f42a34835ef047d9149ee1d92de
Ademir Burgic <aburgic@capeannenterprises.com>
2018-03-02 12:26:50 +0100

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,28 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProtectedComponent } from './protected/protected.component';
 import { HeaderComponent } from './shared/header.component';
+import { routing } from './app.routes';
+import { RouterModule } from '@angular/router';
+
+import { SignupComponent } from "./unprotected/signup.component";
+import { SigninComponent } from "./unprotected/signin.component";
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProtectedComponent,
-    HeaderComponent
+    HeaderComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
