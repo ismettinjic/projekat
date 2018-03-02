a42e0e958a7317d3f24246ffed32eee5714b6a5d
Ademir Burgic <aburgic@capeannenterprises.com>
2018-03-02 14:39:46 +0100

    import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { AppComponent } from './app.component';
 import { ProtectedComponent } from './protected/protected.component';
 import { HeaderComponent } from './shared/header.component';
 import { routing } from './app.routes';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 
 import { SignupComponent } from "./unprotected/signup.component";
 import { SigninComponent } from "./unprotected/signin.component";
+import { AuthService } from './shared/auth.service';
 
 
 @NgModule({
@@ -29,7 +30,7 @@ import { SigninComponent } from "./unprotected/signin.component";
     FormsModule,
     routing
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
